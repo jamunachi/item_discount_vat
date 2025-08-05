66c38aab422b4ef50e5043addddfd956f5f39691
Jamunachi Dev <your.email@domain.com>
2025-08-05 22:50:38 +0300

    Correct setup.py for Frappe app structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='item_discount_vat',
     version='0.0.1',
     description='Apply item-wise discounts with VAT recalculation in ERPNext',
-    author='Your Name',
+    author='jamunachi',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['frappe'],
