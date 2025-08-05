07bf7a593eb13cc3d66122a1fa03cabf8eb1f30f
jamunachi <83000184+jamunachi@users.noreply.github.com>
2025-08-06 00:34:37 +0300

    Update hooks.py

--- item_discount_vat/hooks.py
+++ item_discount_vat/hooks.py
@@ -1,5 +1,12 @@
+app_name = "item_discount_vat"
+app_title = "Item Discount VAT"
+app_publisher = "Jamunachi"
+app_description = "Apply item-wise discounts with VAT recalculation in ERPNext"
+app_email = "jamunachi007@gmail.com"
+app_license = "MIT"
 doc_events = {
     "Sales Invoice": {
         "before_submit": "item_discount_vat.sales_invoice.apply_item_discounts"
     }
 }
+
